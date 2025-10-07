c171e7b716095fd28bf86c15cc718f53f2097c9a
digitalbrainfood <rocketpoweredhosting@gmail.com>
2025-10-07 09:07:43 -0400

    fixed styling

--- .next/server/chunks/ssr/[root-of-the-server]__251ca0b4._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__251ca0b4._.js
@@ -971,7 +971,7 @@ const ProductPage = ({ product, setView, user })=>{
                                 columnNumber: 1651
                             }, ("TURBOPACK compile-time value", void 0)),
                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                                className: "mt-6 border-t pt-4",
+                                className: "mt-6 border-t pt-4 short-description",
                                 children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                                     className: "text-gray-700 prose max-w-none",
                                     dangerouslySetInnerHTML: {
@@ -1084,7 +1084,7 @@ const ProductPage = ({ product, setView, user })=>{
                             columnNumber: 423
                         }, ("TURBOPACK compile-time value", void 0)),
                         isDetailsOpen && /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                            className: "pb-4 text-gray-600 prose max-w-none",
+                            className: "pb-4 text-gray-600 prose max-w-none long-description",
                             dangerouslySetInnerHTML: {
                                 __html: product.longDescription
                             }
--- .next/static/chunks/src_app_page_cf51c7e1.js
+++ .next/static/chunks/src_app_page_cf51c7e1.js
@@ -903,7 +903,7 @@ const ProductPage = (param)=>{
                                 columnNumber: 1651
                             }, ("TURBOPACK compile-time value", void 0)),
                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                                className: "mt-6 border-t pt-4",
+                                className: "mt-6 border-t pt-4 short-description",
                                 children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                                     className: "text-gray-700 prose max-w-none",
                                     dangerouslySetInnerHTML: {
@@ -1016,7 +1016,7 @@ const ProductPage = (param)=>{
                             columnNumber: 423
                         }, ("TURBOPACK compile-time value", void 0)),
                         isDetailsOpen && /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                            className: "pb-4 text-gray-600 prose max-w-none",
+                            className: "pb-4 text-gray-600 prose max-w-none long-description",
                             dangerouslySetInnerHTML: {
                                 __html: product.longDescription
                             }
